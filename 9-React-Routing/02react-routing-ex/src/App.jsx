840e41ce1a208542c928c2c5b59e57e8b454f69e
Shrutika_Dhanad <dhanadshrutika@gmail.com>
2026-03-05 22:17:11 +0530

    routing ex

--- 9-React-Routing/02react-routing-ex/src/App.jsx
+++ 9-React-Routing/02react-routing-ex/src/App.jsx
@@ -16,9 +16,10 @@ function App() {
 <Routes>
 
   <Route  path="/" element={<Home/>}/>
-  <Route  path="/contact" element={<Contact/>}/>
+  <Route  path="/contact" element={<Contact/>} />
 
 </Routes>
+
 </BrowserRouter>
 
 
